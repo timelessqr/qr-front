cd1d2e5e044a30de86da3f525c73c8bbffb271c2
richardggarcia <syoliverts@gmail.com>
2025-06-20 15:43:36 -0300

    Ip local

--- memorial-site/vite.config.js
+++ memorial-site/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 🔧 Esto expone en la red local
+    port: 5173
+  }
 })
